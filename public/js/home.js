15d485624c0e644706b989925c390393c18482f6
khasan0102 <khasanabdukarimov1@gmail.com>
2021-10-04 16:16:37 +0500

    edit renders

--- public/js/home.js
+++ public/js/home.js
@@ -13,12 +13,14 @@ let socket = io("https://task-app-backend-1.herokuapp.com", {
 
 socket.emit("connected", { token });
 
-socket.on("hello", ({ user_id, date }) => {
+socket.on("hello", async ({ user_id, date }) => {
     let elements = document.querySelectorAll(".card-text1");
     let counts = document.querySelectorAll(".btn-primary");
+    let isTrue = true;
     for (let el of elements) {
         if (el.id == user_id) {
             el.textContent = date;
+            isTrue = false;
             break;
         }
     }
@@ -29,57 +31,73 @@ socket.on("hello", ({ user_id, date }) => {
             break;
         }
     }
+
+    if (isTrue) {
+        let response = await fetch("https://task-app-backend-1.herokuapp.com/user/getOne", {
+            method: "GET",
+            headers: {
+                authorization: token
+            }
+        });
+
+    }
 });
+
+
+function makeElement(el, element) {
+    let col = document.createElement("div");
+    let card = document.createElement("div");
+    let img = document.createElement("img");
+    let cardBody = document.createElement("div");
+    let name = document.createElement("h5");
+    let surname = document.createElement("h5");
+    let email = document.createElement("p");
+    let p = document.createElement("p");
+    let span = document.createElement("span");
+    let onlineText = document.createElement("h5");
+
+    col.classList.add("col-md-4");
+    card.classList.add("card");
+    card.style.width = "18rem";
+    cardBody.classList.add("card-body");
+
+    onlineText.setAttribute("id", el._id);
+
+    img.src = el.photo_path;
+    img.classList.add("card-img-top");
+    name.classList.add("card-title");
+    surname.classList.add("card-title");
+    email.classList.add("card-title");
+    p.classList.add("card-text");
+    span.classList = "btn btn-primary";
+    onlineText.classList = "card-text1";
+    name.textContent = el.name;
+    surname.textContent = el.surname;
+    email.textContent = el.email;
+    p.textContent = el.surname;
+    span.textContent = "Count of entries: " + el.count_views;
+    onlineText.textContent = "     ";
+    span.id = el._id
+    span.dataset.count = el.count_views
+
+    cardBody.append(name);
+    cardBody.append(surname);
+    cardBody.append(email);
+    cardBody.append(p);
+    cardBody.append(onlineText);
+    cardBody.append(span);
+
+    card.append(img);
+    card.append(cardBody);
+    col.append(card);
+    element.append(col);
+}
+
 
 function renderElements(array, element) {
     element.innerHTML = null;
     for (let el of array) {
-        let col = document.createElement("div");
-        let card = document.createElement("div");
-        let img = document.createElement("img");
-        let cardBody = document.createElement("div");
-        let name = document.createElement("h5");
-        let surname = document.createElement("h5");
-        let email = document.createElement("p");
-        let p = document.createElement("p");
-        let span = document.createElement("span");
-        let onlineText = document.createElement("h5");
-
-        col.classList.add("col-md-4");
-        card.classList.add("card");
-        card.style.width = "18rem";
-        cardBody.classList.add("card-body");
-
-        onlineText.setAttribute("id", el._id);
-
-        img.src = el.photo_path;
-        img.classList.add("card-img-top");
-        name.classList.add("card-title");
-        surname.classList.add("card-title");
-        email.classList.add("card-title");
-        p.classList.add("card-text");
-        span.classList = "btn btn-primary";
-        onlineText.classList = "card-text1";
-        name.textContent = el.name;
-        surname.textContent = el.surname;
-        email.textContent = el.email;
-        p.textContent = el.surname;
-        span.textContent = "Count of entries: " + el.count_views;
-        onlineText.textContent = "     ";
-        span.id = el._id
-        span.dataset.count = el.count_views
-
-        cardBody.append(name);
-        cardBody.append(surname);
-        cardBody.append(email);
-        cardBody.append(p);
-        cardBody.append(onlineText);
-        cardBody.append(span);
-
-        card.append(img);
-        card.append(cardBody);
-        col.append(card);
-        element.append(col);
+        makeElement(el, element)
     }
 }
 
@@ -102,10 +120,10 @@ socket.on("users", ({ users }) => {
         let counts = document.querySelectorAll(".btn-primary")
         for (let el of elements) {
             if (users[el.id]) {
-                if(users[el.id].id) el.textContent = "online";
+                if (users[el.id].id) el.textContent = "online";
             }
         }
-    },250);
+    }, 250);
 });
 
 getUsers();
